f364425794c0bb27389fe66da3eafa19417cd9aa
AndyKMG <144827552+AndyKMG@users.noreply.github.com>
2024-12-23 06:34:23 +0100

    Add files via upload

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:sms_receiver/sms_receiver.dart';
+import 'package:easy_sms_receiver/easy_sms_receiver.dart';
 
 class Home extends StatefulWidget {
   final Map<String, dynamic> user;
@@ -15,91 +16,109 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final SmsQuery _query = SmsQuery();
-  List<SmsMessage> _filteredMessages = [];
-  String? _textContent = 'Waiting for messages...';
-  SmsReceiver? _smsReceiver;
-  late Timer _timer;
+  // final SmsQuery _query = SmsQuery();
+  // List<SmsMessage> _filteredMessages = [];
+
   @override
   void initState() {
     super.initState();
-    _requestSmsPermission();
-    _smsReceiver = SmsReceiver(onSmsReceived, onTimeout: onTimeout);
-
-    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
-      _startListening(); // This will run every second.
-    });
   }
 
-  void onSmsReceived(String? message) {
-    // Afficher un message de succès ou de redirection
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(" onSmsReceived message ")),
+  // Future<void> _requestSmsPermission() async {
+  //   var status = await Permission.sms.status;
+  //   if (status.isGranted) {
+  //     _fetchSmsMessages();
+  //   } else if (status.isDenied) {
+  //     if (await Permission.sms.request().isGranted) {
+  //       _fetchSmsMessages();
+  //     } else {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         const SnackBar(
+  //             content: Text('Permission refusée pour accéder aux SMS.')),
+  //       );
+  //     }
+  //   } else if (status.isPermanentlyDenied) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(
+  //           content: Text(
+  //               'Permission refusée définitivement. Veuillez accorder l\'accès aux SMS dans les paramètres.')),
+  //     );
+  //   }
+  // }
+
+  // Future<void> _fetchSmsMessages() async {
+  //   List<SmsMessage> messages = await _query.querySms(
+  //     kinds: [SmsQueryKind.inbox],
+  //   );
+  //   setState(() {
+  //     _filteredMessages = messages
+  //         .where((message) =>
+  //             message.body != null &&
+  //             message.body!.startsWith("Transfert effectue pour"))
+  //         .toList();
+  //   });
+  // }
+
+//
+  final EasySmsReceiver easySmsReceiver = EasySmsReceiver.instance;
+  String _easySmsReceiverStatus = "Undefined";
+  String _message = "";
+
+  Future<bool> requestSmsPermission() async {
+    return await Permission.sms.request().then(
+      (PermissionStatus pStatus) {
+        if (pStatus.isPermanentlyDenied) {
+          // "You must allow sms permission"
+          openAppSettings();
+        }
+        return pStatus.isGranted;
+      },
     );
-    print(" onSmsReceived message");
-    setState(() {
-      _textContent = message;
-    });
   }
 
-  void onTimeout() {
-    setState(() {
-      _textContent = 'Timeout!!!';
-    });
-  }
+  Future<void> startSmsReceiver() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    if (await requestSmsPermission()) {
+      easySmsReceiver.listenIncomingSms(
+        onNewMessage: (message) {
+          print("You have new message:");
+          print("::::::Message Address: ${message.address}");
+          print("::::::Message body: ${message.body}");
+          if (message.body != null &&
+              message.body!.startsWith("Transfert effectue pour")){
+                Fluttertoast.showToast(msg: "::::::Message body: ${message.body}");
+              }
+              
+               if (!mounted)
+            return;
 
-  void _startListening() async {
-    print("_startListening was called ");
-    if (_smsReceiver == null) return;
-    await _smsReceiver?.startListening();
-    setState(() {
-      _textContent = 'Waiting for messages...';
-    });
-  }
-
-  void _stopListening() async {
-    if (_smsReceiver == null) return;
-    await _smsReceiver?.stopListening();
-    setState(() {
-      _textContent = 'Listener Stopped';
-    });
-  }
-
-  Future<void> _requestSmsPermission() async {
-    var status = await Permission.sms.status;
-    if (status.isGranted) {
-      _fetchSmsMessages();
-    } else if (status.isDenied) {
-      if (await Permission.sms.request().isGranted) {
-        _fetchSmsMessages();
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-              content: Text('Permission refusée pour accéder aux SMS.')),
-        );
-      }
-    } else if (status.isPermanentlyDenied) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content: Text(
-                'Permission refusée définitivement. Veuillez accorder l\'accès aux SMS dans les paramètres.')),
+          setState(() {
+            _message = message.body ?? "Error reading message body.";
+          });
+        },
       );
+
+      // If the widget was removed from the tree while the asynchronous platform
+      // message was in flight, we want to discard the reply rather than calling
+      // setState to update our non-existent appearance.
+      if (!mounted) return;
+
+      setState(() {
+        _easySmsReceiverStatus = "Running";
+      });
     }
   }
 
-  Future<void> _fetchSmsMessages() async {
-    List<SmsMessage> messages = await _query.querySms(
-      kinds: [SmsQueryKind.inbox],
-    );
+  void stopSmsReceiver() {
+    easySmsReceiver.stopListenIncomingSms();
+    if (!mounted) return;
+
     setState(() {
-      _filteredMessages = messages
-          .where((message) =>
-              message.body != null &&
-              message.body!.startsWith("Transfert effectue pour"))
-          .toList();
+      _easySmsReceiverStatus = "Stopped";
     });
   }
 
+//
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -126,26 +145,37 @@ class _HomeState extends State<Home> {
               style: const TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 30),
-            Text(_textContent ?? 'empty'),
-            ElevatedButton(
-              onPressed: _fetchSmsMessages,
-              child: const Text('Afficher les SMS filtrés'),
-            ),
+            // ElevatedButton(
+            //   onPressed: _fetchSmsMessages,
+            //   child: const Text('Afficher les SMS filtrés'),
+            // ),
             const SizedBox(height: 20),
-            Expanded(
-              child: _filteredMessages.isEmpty
-                  ? const Text('Aucun SMS filtré trouvé.')
-                  : ListView.builder(
-                      itemCount: _filteredMessages.length,
-                      itemBuilder: (context, index) {
-                        final message = _filteredMessages[index];
-                        return ListTile(
-                          title: Text(message.body ?? ""),
-                          subtitle: Text(message.date.toString()),
-                        );
-                      },
-                    ),
-            ),
+            Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Latest Received SMS: $_message"),
+                Text('EasySmsReceiver Status: $_easySmsReceiverStatus\n'),
+                TextButton(
+                    onPressed: startSmsReceiver, child: Text("Start Receiver")),
+                TextButton(
+                    onPressed: stopSmsReceiver, child: Text("Stop Receiver")),
+              ],
+            )),
+            // Expanded(
+            //   child: _filteredMessages.isEmpty
+            //       ? const Text('Aucun SMS filtré trouvé.')
+            //       : ListView.builder(
+            //           itemCount: _filteredMessages.length,
+            //           itemBuilder: (context, index) {
+            //             final message = _filteredMessages[index];
+            //             return ListTile(
+            //               title: Text(message.body ?? ""),
+            //               subtitle: Text(message.date.toString()),
+            //             );
+            //           },
+            //         ),
+            // ),
           ],
         ),
       ),
